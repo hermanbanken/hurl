ecd7257f3ec05eef9eefbfd5520ace6447235db9
Herman <herman@q42.nl>
2023-09-19 14:29:21 +0200

    better max-rate setting that incorporates the time taken

--- main.go
+++ main.go
@@ -17,11 +17,13 @@ import (
 )
 
 var (
-	parallelism = atomic.Int64{} // parallelism
-	rate        = atomic.Int64{} // max-rate per worker
-	input       = make(chan string)
-	workers     = atomic.Int64{}
-	wg          = sync.WaitGroup{}
+	parallelism   = &atomic.Int64{} // parallelism
+	rate          = &atomic.Int64{} // max-rate per worker
+	input         = make(chan string)
+	workers       = &atomic.Int64{}
+	wg            = sync.WaitGroup{}
+	perTryTimeout = 10 * time.Second
+	retries       = 3
 )
 
 func main() {
@@ -38,35 +40,17 @@ func main() {
 		inScanner.Split(bufio.ScanLines)
 		for inScanner.Scan() {
 			text := strings.TrimSpace(inScanner.Text())
-			// usage, to control parallelism, write: p=100
-			if rest := strings.TrimPrefix(text, "p="); strings.HasPrefix(text, "p=") {
-				p, err := strconv.ParseInt(rest, 10, 64)
-				if err == nil {
-					parallelism.Store(p)
-					log.Printf("Set parallelism to %d", p)
-					current := workers.Load()
-					if p > current {
-						for i := current; i < p; i++ {
-							go startWorker()
-						}
-					}
-				} else {
-					log.Println(err)
-				}
-				continue
+			if true &&
+				// usage, to control parallelism, write: p=100
+				!setting("p", "parallelism", text, atomicIntSetting{parallelism}) &&
+				// usage, to control the max-rate per worker, set to 16/s, write: r=16
+				!setting("r", "rate", text, atomicIntSetting{rate}) &&
+				// usage, to control the per try timeout, write: t=10s
+				!setting("t", "timeout", text, durationSetting{&perTryTimeout}) &&
+				// usage, to control the retries, write: c=3
+				!setting("c", "retries", text, intSetting{&retries}) {
+				log.Println("unknown command", text)
 			}
-			// usage, to control the max-rate per worker, set to 16/s, write: r=16
-			if rest := strings.TrimPrefix(text, "r="); strings.HasPrefix(text, "r=") {
-				r, err := strconv.ParseInt(rest, 10, 64)
-				if err == nil {
-					rate.Store(r)
-					log.Printf("Set rate to %d/s", r)
-				} else {
-					log.Println(err)
-				}
-				continue
-			}
-			log.Println("unknown command", text)
 		}
 	}()
 
@@ -103,6 +87,7 @@ func startWorker() {
 	workRate := rate.Load()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	var last time.Time
 	for {
 		select {
 		case <-t.C:
@@ -114,16 +99,79 @@ func startWorker() {
 			if !ok {
 				return
 			}
-			rctx, rcancel := context.WithTimeout(ctx, 10*time.Second)
-			defer rcancel()
-			req, _ := http.NewRequestWithContext(rctx, "GET", url, nil)
-			resp, err := http.DefaultClient.Do(req)
+			// enforce max speed
+			if time.Since(last) < time.Second/time.Duration(workRate) {
+				time.Sleep(time.Second/time.Duration(workRate) - time.Since(last))
+			}
+			last = time.Now()
+
+			resp, err := doReq(ctx, url)
 			if err != nil {
 				fmt.Println(url, errors.Wrap(err, "http"))
 			} else {
 				fmt.Println(url, resp.StatusCode)
 			}
-			time.Sleep(time.Second / time.Duration(workRate))
 		}
 	}
+}
+
+func doReq(ctx context.Context, url string) (resp *http.Response, err error) {
+	for i := retries; i > 0; i-- {
+		rctx, rcancel := context.WithTimeout(ctx, perTryTimeout)
+		defer rcancel()
+		req, _ := http.NewRequestWithContext(rctx, "GET", url, nil)
+		resp, err = http.DefaultClient.Do(req)
+		if err == nil {
+			return resp, nil
+		}
+	}
+	return
+}
+
+func setting(short, name, value string, storage storage) bool {
+	if rest := strings.TrimPrefix(value, short+"="); strings.HasPrefix(value, short+"=") {
+		if err := storage.Store(rest); err != nil {
+			log.Println(err)
+			return false
+		} else {
+			log.Printf("Set %s to %s", name, rest)
+			return true
+		}
+	}
+	return false
+}
+
+type storage interface {
+	Store(string) error
+}
+
+type atomicIntSetting struct {
+	*atomic.Int64
+}
+
+func (a atomicIntSetting) Store(val string) (err error) {
+	i, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return err
+	}
+	a.Int64.Store(i)
+	return
+}
+
+type durationSetting struct {
+	*time.Duration
+}
+
+func (d durationSetting) Store(val string) (err error) {
+	*d.Duration, err = time.ParseDuration(val)
+	return
+}
+
+type intSetting struct {
+	*int
+}
+
+func (d intSetting) Store(val string) (err error) {
+	*d.int, err = strconv.Atoi(val)
+	return
 }
